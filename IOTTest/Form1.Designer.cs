48a29e3d0c4a056a95f48bd7ddb46d9e9667c34b
Dasith@4463 <daiths@hotmail.com>
2017-01-03 16:11:17 +1100

    .

--- IOTTest/Form1.Designer.cs
+++ IOTTest/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAddDevice = new System.Windows.Forms.Button();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnCancelListening = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddDevice
+            // 
+            this.btnAddDevice.Location = new System.Drawing.Point(25, 16);
+            this.btnAddDevice.Name = "btnAddDevice";
+            this.btnAddDevice.Size = new System.Drawing.Size(126, 29);
+            this.btnAddDevice.TabIndex = 0;
+            this.btnAddDevice.Text = "&Add Device";
+            this.btnAddDevice.UseVisualStyleBackColor = true;
+            this.btnAddDevice.Click += new System.EventHandler(this.btnAddDevice_Click);
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(25, 81);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(78, 26);
+            this.btnListen.TabIndex = 2;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(157, 16);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(439, 272);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // btnCancelListening
+            // 
+            this.btnCancelListening.Location = new System.Drawing.Point(25, 113);
+            this.btnCancelListening.Name = "btnCancelListening";
+            this.btnCancelListening.Size = new System.Drawing.Size(126, 27);
+            this.btnCancelListening.TabIndex = 4;
+            this.btnCancelListening.Text = "&Cancel Listening";
+            this.btnCancelListening.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(25, 176);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(108, 27);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "&Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 300);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnCancelListening);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.btnAddDevice);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAddDevice;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnCancelListening;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
--- IOTTest/Form1.cs
+++ IOTTest/Form1.cs
@@ -5,16 +5,138 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using Message = Microsoft.Azure.Devices.Client.Message;
+using TransportType = Microsoft.Azure.Devices.Client.TransportType;
 
 namespace IOTTest
 {
     public partial class Form1 : Form
     {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=DasithIOTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ShSXR0KjrGZL98aSiTWh/IrJTxBYo6AIfbXJQvnVGwU=";
+
+        string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+
+        static DeviceClient deviceClient;
+        static string iotHubUri = "DasithIOTHub.azure-devices.net";
+        static string deviceKey = "ChOZOP8GkluY2KkjeCEOXnIqugGlpW4yNbj/i9N65HI=";
+
         public Form1()
         {
             InitializeComponent();
+
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
         }
+
+        private void WriteToOutput(string s)
+        {
+            txtOutput.Text = txtOutput.Text + Environment.NewLine + s;
+            txtOutput.SelectionStart = txtOutput.Text.Length - 1;
+        }
+
+        private async void btnAddDevice_Click(object sender, EventArgs e)
+        {
+
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+
+            WriteToOutput("Generated device key: " + device.Authentication.SymmetricKey.PrimaryKey);
+        }
+
+        #region "Recieve"
+
+        private async void btnListen_Click(object sender, EventArgs e)
+        {
+            btnListen.Enabled = false;
+
+            WriteToOutput("Receive messages.");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            btnCancelListening.Click += (s, a) =>
+            {
+                cts.Cancel();
+                WriteToOutput("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                WriteToOutput($"Message received. Partition: {partition} Data: '{data}'");
+            }
+        }
+
+        #endregion
+
+        #region "Send"
+
+        private async Task SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "myFirstDevice",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                WriteToOutput($"{DateTime.Now} > Sending message: {messageString}");
+
+                Task.Delay(1000).Wait();
+            }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            WriteToOutput("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Mqtt);
+
+            await SendDeviceToCloudMessagesAsync();
+        }
+
+        #endregion
     }
 }
