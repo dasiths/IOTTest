871fb773a42935c81f001ef1ac56fcf00f1b8c93
Dasith Wijesiriwardena <dasiths@hotmail.com>
2017-05-24 17:27:54 +1000

    Update Form1.cs

--- IOTTest/Form1.cs
+++ IOTTest/Form1.cs
@@ -21,6 +21,8 @@ namespace IOTTest
 {
     public partial class Form1 : Form
     {
+    
+        //See tutorial at https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-csharp-csharp-getstarted
         static RegistryManager registryManager;
         static string connectionString = "HostName=DasithIOTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ShSXR0KjrGZL98aSiTWh/IrJTxBYo6AIfbXJQvnVGwU=";
 
